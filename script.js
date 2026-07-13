acaed21c78a4e4c7ce9defe7d12988cf21939ca9
anashossainios16-create <anashossainios16@gmail.com>
2026-07-13 17:00:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-let count = 0;
+let cart = 0;
 
-document.querySelectorAll(".card").forEach(card => {
-  card.addEventListener("click", () => {
-    count++;
-    document.getElementById("cart-count").textContent = count;
+const cartCount = document.getElementById("cart-count");
+const buttons = document.querySelectorAll(".card button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    cart++;
+    cartCount.textContent = cart;
+
+    button.innerText = "Added ✓";
+
+    setTimeout(() => {
+      button.innerText = "Add to Cart";
+    }, 1000);
+  });
+});
+
+const search = document.querySelector(".search");
+
+search.addEventListener("keyup", () => {
+  const value = search.value.toLowerCase();
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    const title = card.querySelector("h3").innerText.toLowerCase();
+
+    if (title.includes(value)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
   });
 });
