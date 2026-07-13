1766a4abc29e78eb019330fb2b894c14827dbe91
anashossainios16-create <anashossainios16@gmail.com>
2026-07-13 16:47:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
+let count = 0;
 
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-    card.style.cursor = "pointer";
-
-    card.addEventListener("click", () => {
-        alert("এই প্রোডাক্টটি খুব শীঘ্রই যোগ করা হবে।");
-    });
-});
-
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("click", () => {
+    count++;
+    document.getElementById("cart-count").textContent = count;
+  });
 });
