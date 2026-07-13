ef422d5ec3dd60b7f367d470296121cf1898d7c5
anashossainios16-create <anashossainios16@gmail.com>
2026-07-13 16:43:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// Hopper Mart Script
-console.log("Hopper Mart Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.style.cursor = "pointer";
+
+    card.addEventListener("click", () => {
+        alert("এই প্রোডাক্টটি খুব শীঘ্রই যোগ করা হবে।");
+    });
+});
+
+});
